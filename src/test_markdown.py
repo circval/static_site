658e67923786a89b333d5f6c7aa21454ba98a23a
circval <noah.arnett1@gmail.com>
2026-06-30 09:48:44 -0700

    6/30/26 - added block_to_block_type tests

--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestMarkdown(unittest.TestCase):
     def test_markdown_to_blocks_1(self):
@@ -61,5 +61,49 @@ and an extra line
                 "and an extra line"
             ],
         )
+        
+    def test_block_to_blocktype_paragraph(self):
+        block = """This is a line.\n,
+This is the second line."""   
+
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+    
+    def test_block_to_blocktype_heading(self):
+        block = """# This\n,
+## is\n,
+### a\n,
+#### heading."""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.HEADING)
+        
+    def test_block_to_blocktype_code(self):
+        block = """```\nCode```,
+```\ngoes```,
+```\nhere.```"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.CODE)
+    
+    def test_block_to_blocktype_quote(self):
+        block = """> Someone,
+>is being,
+> quoted"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+        
+    def test_block_to_blocktype_unordered(self):
+        block = """- This,
+- is an,
+- unordered list"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+        
+    def test_block_to_blocktype_ordered(self):
+        block = """1. This,
+2. is an,
+3. ordered list"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+        
 if __name__ == "__main__":
         unittest.main()
